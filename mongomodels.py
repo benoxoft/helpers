433aa3837bbc32658f86b7b01ec3a99144ff3f3e
Fabian Trautsch <fabian.trautsch@informatik.uni-goettingen.de>
2016-11-18 16:46:35 +0100

    new mongo classes

--- mongomodels.py
+++ mongomodels.py
@@ -436,3 +436,105 @@ class NodeTypeCount(Document):
     fileId = ObjectIdField(required=True, unique_with=['commitId'])
     nodeCount = IntField()  # full number of nodes
     nodeTypeCounts = DictField() # every node with its number for this file
+
+
+class FileState(Document):
+    """ Document that inherits from :class:`mongoengine.Document`. Holds information for the file_state collection.
+
+    :property commit_id: id of the commit to which the file state belongs to (type: :class:`mongoengine.fields.ObjectIdField`)
+    :property file_id: id of the file to which the state belongs (type: :class:`mongoengine.fields.ObjectIdField`)
+    :property long_name: long_name of the state entity (e.g., pyvcssharhk.main.py) (type: :class:`mongoengine.fields.StringField`)
+    :property component_ids: component_ids to which this entity belongs (type: :class:`mongoengine.fields.ListField` of :class:`mongoengine.fields.ObjectFieldId`)
+    :property name: name of the state entity (type: :class:`mongoengine.fields.StringField`)
+    :property file_type: file_type of the state entity (e.g., class, attribute, method, function, ...) (type: :class:`mongoengine.fields.StringField`)
+    :property parent: parent of the state entity (e.g., if the entity is a method, the parent can be a class) (type: :class:`mongoengine.fields.ObjectFieldId`)
+    :property metrics: metrics of the state entity (type: :class:`mongoengine.fields.DictField`)
+
+    .. NOTE:: Unique (or primary keys) are the fields commit_id and file_id.
+    """
+    meta = {
+        'indexes': [
+            'commit_id',
+            'file_id',
+        ],
+        'shard_key': ('commit_id', 'file_id', 'long_name'),
+    }
+
+    file_id = ObjectIdField(required=True)
+    commit_id = ObjectIdField(required=True, unique_with=['file_id', 'long_name'])
+    parent = ObjectIdField()
+    component_ids = ListField(ObjectIdField())
+    long_name = StringField(required=True)
+    name = StringField()
+    file_type = StringField()
+    startLine = IntField()
+    endLine = IntField()
+    startColumn = IntField()
+    endColumn = IntField()
+    metrics = DictField()
+
+
+class MetaPackageState(Document):
+    """ Document that inherits from :class:`mongoengine.Document`. Holds information for the meta_package_state collection.
+    Here information about a collection of file_states is saved (e.g. packages) that do not belong to one file, but
+    have associations to more than one file.
+
+    :property commit_id: id of the commit to which the meta package state belongs (type: :class:`mongoengine.fields.ObjectIdField`)
+    :property long_name: long_name of the state entity (type: :class:`mongoengine.fields.StringField`)
+    :property name: name of the state entity (type: :class:`mongoengine.fields.StringField`)
+    :property component_ids: component_ids of the state entity (e.g., pyvcssharhk.main.py) (type: :class:`mongoengine.fields.StringField`)
+    :property parent_state: parent of the state entity  (type: :class:`mongoengine.fields.ObjectFieldId`)
+    :property metrics: metrics of the state entity (type: :class:`mongoengine.fields.DictField`)
+
+    .. NOTE:: Unique (or primary keys) are the fields commit_id and long_name.
+    """
+    meta = {
+        'indexes': [
+            'commit_id'
+        ],
+        'shard_key': ('commit_id', 'long_name'),
+    }
+
+    commit_id = ObjectIdField(required=True, unique_with=['long_name'])
+    parent_state = ObjectIdField()
+    component_ids = ListField(ObjectIdField())
+    long_name = StringField(require=True)
+    name = StringField()
+    file_type = StringField()
+    metrics = DictField()
+
+
+class CloneInstance(Document):
+    """ Document that inherits from :class:`mongoengine.Document`. Holds information for the clone_instance collection.
+
+    :property commit_id: id of the commit, where the clone was detected (type: :class:`mongoengine.fields.ObjectIdField`)
+    :property name: name of the clone entity (type: :class:`mongoengine.fields.StringField`)
+    :property fileId: id of the file to which the clone belongs (type: :class:`mongoengine.fields.ObjectIdField`)
+    :property cloneClass: name of the clone class  (type: :class:`mongoengine.fields.StringField`)
+    :property cloneClassMetrics: metrics of the clone class (type: :class:`mongoengine.fields.DictField`)
+    :property cloneInstanceMetrics: metrics of the clone class (type: :class:`mongoengine.fields.DictField`)
+    :property startLine: start line of the clone (type: :class:`mongoengine.fields.IntField`)
+    :property endLine: end line of the clone (type: :class:`mongoengine.fields.IntField`)
+    :property startColumn: start column of the clone (type: :class:`mongoengine.fields.IntField`)
+    :property endColumn: end column of the clone (type: :class:`mongoengine.fields.IntField`)
+
+    .. NOTE:: Unique (or primary keys) are the fields commit_id, name, and fileId.
+    """
+    meta = {
+        'indexes': [
+            'commit_id',
+            'fileId',
+        ],
+        'shard_key': ('commit_id', 'fileId', 'name'),
+    }
+
+    commit_id = ObjectIdField(required=True, unique_with=['fileId', 'name'])
+    fileId = ObjectIdField(required=True)
+    name = StringField(required=True)
+    startLine = IntField(required=True)
+    endLine = IntField(required=True)
+    startColumn = IntField(required=True)
+    endColumn = IntField(required=True)
+    cloneInstanceMetrics = DictField(required=True)
+    cloneClass = StringField(required=True)
+    cloneClassMetrics = DictField(required=True)
